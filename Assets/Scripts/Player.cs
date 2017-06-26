420ff0d86f1cee402a0e9c8850210d095812bab7
Jerry Feng <jerry.feng41@gmail.com>
2017-06-25 19:21:32 -0700

    turning

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,17 +8,25 @@ public class Player : MonoBehaviour {
 
 	Vector3 movement;
 	Rigidbody body;
+	GameObject playerModel;
 
 	void Awake() {
 		body = GetComponent<Rigidbody>();
+		playerModel = this.transform.Find("Model").gameObject;
 	}
 	
 	void FixedUpdate() {
 		float h = Input.GetAxisRaw("Horizontal");
 		float v = Input.GetAxisRaw("Vertical");
 
-		movement.Set(h, 0f, v);
-		movement = Quaternion.Euler(0, 45, 0) * movement.normalized * moveSpeed * Time.deltaTime;
-		body.MovePosition(transform.position + movement);
+		if (h != 0 || v != 0) {
+			movement.Set(h, 0f, v);
+			movement = Quaternion.Euler(0, 45, 0) * movement.normalized * moveSpeed * Time.deltaTime;
+			body.MovePosition(transform.position + movement);
+
+			Vector3 rotation = playerModel.transform.rotation.eulerAngles;
+			rotation.y = Quaternion.LookRotation(movement).eulerAngles.y;
+			playerModel.transform.eulerAngles = rotation;
+		}		
 	}
 }
