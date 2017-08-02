8f030337163bff55643593705314df93c001147f
Jerry Feng <jerry.feng41@gmail.com>
2017-08-01 22:17:13 -0700

    remove slow lock on running attacks, lerp player rotation

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
-	public float moveSpeed = 8f;
-	public float attackMoveSpeedFactor = 0.4f;
+	public float moveSpeed;
+	public float rotationSmoothing;
 
 	private Vector3 movement;
 	private Rigidbody body;
@@ -32,20 +32,18 @@ public class PlayerController : MonoBehaviour {
 		float v = Input.GetAxisRaw("Vertical");
 
 		if (h != 0 || v != 0) {
-			float realMoveSpeed = isAttacking ?
-					moveSpeed * attackMoveSpeedFactor :
-					moveSpeed;
 			movement.Set(h, 0f, v);
-			movement = Quaternion.Euler(0, 45, 0) * movement.normalized * realMoveSpeed * Time.deltaTime;
+			movement = Quaternion.Euler(0, 45, 0) * movement.normalized * moveSpeed * Time.deltaTime;
 			body.MovePosition(transform.position + movement);
-
-			Vector3 rotation = this.transform.rotation.eulerAngles;
-			rotation.y = Quaternion.LookRotation(movement).eulerAngles.y;
-			this.transform.eulerAngles = rotation;
 
 			animator.SetBool("Running", true);
 		} else {
 			animator.SetBool("Running", false);
+		}
+
+		if (movement != Vector3.zero) {
+			Quaternion newRot = Quaternion.LookRotation(movement);
+			transform.rotation = Quaternion.Lerp(transform.rotation, newRot, rotationSmoothing * Time.deltaTime);
 		}
 	}
 
